82b4a997874cdc3ec10b541711b5069fe829e7b4
toushi-muzu <contact@toushi-muzu.com>
2019-11-24 21:23:23 +0900

    modelにUsstockクラスを作成

--- usstocklist/models.py
+++ usstocklist/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Market(models.Model):
-  market = models.CharField('市場', max_length=255)
+  market = models.CharField('市場', max_length=10)
 
   def __str__(self):
     return self.market
+
+class Usstock(models.Model):
+  ticker = models.CharField('ティッカー', max_length=5)
+  stockname = models.CharField('銘柄名', max_length=255)
+  market = models.ForeignKey(
+    Market, verbose_name='市場', on_delete=models.PROTECT,
+  )
+
+  def __str__(self):
+    return '{0} {1} {2}'.format(self.ticker, self.stockname, self.exchange)
