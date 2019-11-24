da6525c00155b04153377350432616cdf3d978ff
toushi-muzu <contact@toushi-muzu.com>
2019-11-25 00:11:44 +0900

    変更機能を追加

--- usstocklist/urls.py
+++ usstocklist/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+"""
+プライマリーキーが文字列の場合slug:pkと指定する
+"""
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('usstocklist/add/', views.AddView.as_view(), name='add'),
+  path('', views.IndexView.as_view(), name='index'),
+  path('usstocklist/add/', views.AddView.as_view(), name='add'),
+  path('usstocklist/edit/<slug:pk>', views.EditView.as_view(), name='edit'),
 ]
--- usstocklist/views.py
+++ usstocklist/views.py
@@ -3,16 +3,30 @@ from django.views import generic
 from django.urls import reverse_lazy
 from .models import Usstock
 
+"""
+汎用ビュー(ListView)を使う場合
+templates以下のフォルダ名はアプリ名と同じ名前⇒usstocklist
+フォルダ以下のhtml名はモデルのクラス名_list.htmlとする⇒usstock_list.html
+"""
 class IndexView(generic.ListView):
   model = Usstock
 
 """
-汎用ビューを使う場合
-templates以下のフォルダ名はアプリ名と同じ名前⇒usstocklist
-フォルダ以下のhtml名はモデルのクラス名_list.htmlとする⇒usstock_list.html
+汎用ビュー(CreateView)を使う場合
+フォルダ以下のhtml名はモデルのクラス名_form.htmlとする⇒usstock_form.html
 """
-
 class AddView(generic.CreateView):
   model = Usstock
   fields = '__all__'
   success_url = reverse_lazy('index')
+
+"""
+汎用ビュー(UpdateView)を使う場合
+フォルダ以下のhtml名はモデルのクラス名_list.htmlとする⇒usstock_form.htmlとすべきだが
+登録画面と同じになるので新たに変更画面(usstock_form.html)を作成しtemplate_nameで指定する 
+"""
+class EditView(generic.UpdateView):
+  model = Usstock
+  fields = '__all__'
+  success_url = reverse_lazy('index')
+  template_name = 'usstocklist/usstock_edit.html'
