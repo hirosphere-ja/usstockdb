789fbaf85724ab46c615f5bf3fde4147e141d44d
toushi-muzu <contact@toushi-muzu.com>
2019-11-24 23:03:04 +0900

    AddViewクラスを追加

--- usstocklist/urls.py
+++ usstocklist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('usstocklist/add/', views.AddView.as_view(), name='add'),
 ]
--- usstocklist/views.py
+++ usstocklist/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import Usstock
 
 class IndexView(generic.ListView):
-    model = Usstock
+  model = Usstock
 
 """
 汎用ビューを使う場合
 templates以下のフォルダ名はアプリ名と同じ名前⇒usstocklist
 フォルダ以下のhtml名はモデルのクラス名_list.htmlとする⇒usstock_list.html
 """
+
+class AddView(generic.CreateView):
+  model = Usstock
+  fields = '__all__'
+  success_url = reverse_lazy('index')
