3694fee2d66ac292a11bbe0aa2d990b06eb27623
toushi-muzu <contact@toushi-muzu.com>
2019-11-24 21:54:11 +0900

    MarketからUsstockに変更

--- usstocklist/views.py
+++ usstocklist/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Market
+from .models import Usstock
 
 class IndexView(generic.ListView):
-    model = Market
+    model = Usstock
 
 """
 汎用ビューを使う場合
-templates以下のフォルダ名はアプリ名と同じ名前⇒usstockmarket
-フォルダ以下のhtml名はモデルのクラス名_list.htmlとする⇒market_list.html
+templates以下のフォルダ名はアプリ名と同じ名前⇒usstocklist
+フォルダ以下のhtml名はモデルのクラス名_list.htmlとする⇒usstock_list.html
 """
